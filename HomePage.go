064f13807aca473a80d9f2a5cb55b766ebe22e48
re-incarnation <74276034+re-incarnation@users.noreply.github.com>
2021-10-18 11:18:37 +1000

    Update HomePage.go

--- HomePage.go
+++ HomePage.go
@@ -10,6 +10,20 @@ import (
 
 
 func HomePage(page http.ResponseWriter, req *http.Request) {
+		store, err := session.Start(context.Background(), page, req)
+	if err != nil {
+		fmt.Fprint(page, err)
+		return
+	}
+
+
+	active, ok := store.Get("active_login")
+	if ok {
+		fmt.Fprintf(page, "Nice session %s", active)
+	}else{
+		fmt.Fprintf(page, "Error session" )
+	}
+
 	temp, err := template.ParseFiles("templates/html/HomePage.html")
 
 	if err != nil {
